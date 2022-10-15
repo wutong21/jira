054310e0c97ea8d49eb29f6940ba284fb56f310e
wutong21 <18072916289@163.com>
2022-10-15 22:30:33 +0800

    feat: 添加登录状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,6 +4,7 @@ import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 import { cleanObject, useDebounce, useMount } from '../../utils'
 import * as qs from "qs"
+import { useHttp } from '../../utils/http';
 
 const apiUrl = process.env.REACT_APP_API_URL
 // 使用typescript的原因 在静态代码中找到其中的一些错误 -> 强类型
@@ -16,21 +17,14 @@ export const ProjectListScreen = () => {
     })
     const debouncedParam = useDebounce(param, 200)
     const [list, setList] = useState([])
+    const client = useHttp()
 
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
-            if (response.ok) {
-                setList(await response.json())
-            }
-        })
+        client('projects', { data: cleanObject(debouncedParam) }).then(setList)
     }, [debouncedParam])
 
     useMount(() => {
-        fetch(`${apiUrl}/users`).then(async response => {
-            if (response.ok) {
-                setUsers(await response.json())
-            }
-        })
+        client('users').then(setUsers)
     })
     return <div>
         <SearchPanel users={users} param={param} setParam={setParam} />
