0835782cc150f818a4584cc08a05e304515eb134
wutong21 <18072916289@163.com>
2022-10-22 18:37:16 +0800

    style: 用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import React from 'react';
+import { Row } from './components/lib';
 import { useAuth } from './context/auth-context';
 
 import { ProjectListScreen } from "./screens/project-list"
@@ -17,49 +18,33 @@ import { ProjectListScreen } from "./screens/project-list"
 export const AuthenticatedApp = () => {
     const { logout } = useAuth()
     return <Container>
-        <Header>
-            <HeaderLeft>
-                <h3>LOGO</h3>
-                <h3>项目</h3>
-                <h3>用户</h3>
+        <Header between={true}>
+            <HeaderLeft gap={true}>
+                <h2>LOGO</h2>
+                <h2>项目</h2>
+                <h2>用户</h2>
             </HeaderLeft>
             <HeaderRight>
                 <button onClick={logout}>登出</button>
             </HeaderRight>
         </Header>
-        <Nav>nav</Nav>
         <Main>
             <ProjectListScreen />
         </Main>
-        <Aside>aside</Aside>
-        <Footer>footer</Footer>
     </Container>
 }
 
+const HeaderItemm = styled.h3`
+margin-right: 3rem`
+
 const Container = styled.div`
-    display: grid;
-    grid-template-rows: 6rem 1fr 6rem;
-    grid-template-columns: 20rem 1fr 20rem;
-    grid-template-areas: 
-    "header header header"
-    "nav main aside"
-    "footer footer footer";
     height: 100vh;
-    grid-gap: 10rem;
 `
 
-const Header = styled.header`
-grid-area:header;
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: space-between ;
+const Header = styled(Row)`
 `
-const HeaderLeft = styled.div`
+const HeaderLeft = styled(Row)`
 display:flex;
 align-items: center`
 const HeaderRight = styled.div``
 const Main = styled.main`grid-area:main`
-const Nav = styled.nav`grid-area:nav`
-const Aside = styled.aside`grid-area:aside`
-const Footer = styled.footer`grid-area:footer`
